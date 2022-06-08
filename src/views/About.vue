5536d57db81c0a1c8d7d7774c1379ebba54d6ab0
AlanHong <show910615@gmail.com>
2022-06-08 17:34:42 +0800

    learnVuex

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,38 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="aaa">+1</button>
   </div>
 </template>
 
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: "App",
+  data(){
+    return {
+      localCount:'aaa'
+    }
+  },
+  methods: {
+    aaa() {
+      this.$store.commit("increment");
+      console.log(this.$store.state.count);
+    },
+  },
+  computed: mapState({
+    // 箭头函数可使代码更简练
+    count: state => state.count,
+    countPlusLocalState(state) {
+      return state.count + this.localCount
+    }
+  })
+};
+</script>
+
 <style lang="scss" scoped>
-  h1{
-    color: red;
-  }
+h1 {
+  color: red;
+}
 </style>
